41328226c8b10f0beb5ffac2c10bd8dac566febc
thanhta <thanhta2010@gmail.com>
2024-01-01 20:48:46 -0500

    update conda.yml and add codes in main.py for data_split step

--- main.py
+++ main.py
@@ -70,13 +70,35 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "cleaned_sample.csv:latest",
+                    "ref": "cleaned_sample.csv:reference",
+                    "kl_threshold": config['data_check']['kl_threshold'],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
+            
 
         if "data_split" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                version='main',
+                parameters={
+                    "input": "cleaned_sample.csv:latest",
+                    "test_size": config['modeling']['test_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by']
+                },
+            )
+                
 
         if "train_random_forest" in active_steps:
 
